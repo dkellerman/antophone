5125074d6a6bc71cedd1d2f838f67b133bf08d29
David Kellerman <david@silvermoon.org>
2022-11-29 12:14:46 -0500

    farm.py

---
+++ farm.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import sys
+import traceback
+from antophone.farm import Farm
+
+if __name__ == '__main__':
+    farm = Farm()
+    try:
+        farm.run_user_session()
+    except Exception as ex:
+        print(''.join(traceback.format_exception(etype=type(ex), value=ex, tb=ex.__traceback__)))
+    finally:
+        farm.quit()
+        sys.exit(0)
